818aa4e089864a2211c3f4e971ac396f9087cfc0
Maykon William <desenvolvimento@mmd.com.br>
2025-06-07 19:39:08 -0300

    Testes das chamadas dos metodos do Cursos

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.util.List;
+
+import adapters.database.csv.CursoCSVRepositoryAdapter;
+import domain.entities.Curso;
 
 public class Main {
 
@@ -6,7 +10,55 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		CursoCSVRepositoryAdapter repository = new CursoCSVRepositoryAdapter("cursos.csv");
+
+		// CREATE
+		Curso curso = new Curso(1, "ADS", "T.I");
+		repository.save(curso);
+
+		curso = new Curso(2, "Teste", "T.I");
+		repository.save(curso);
+		System.out.println("--------------FIM CREATE");
+
+		// LIST
+		List<Curso> cursos = repository.list();
+		for (Curso cursoItem : cursos) {
+			System.out.println(cursoItem.getNome());
+		}
+		System.out.println("--------------FIM LIST");
+
+		// READ
+		curso = repository.show(1);
+		if (curso != null) {
+			System.out.println(curso.getNome());
+		}
+		System.out.println("--------------FIM READ");
+
+		// Update
+		curso = repository.show(1);
+		System.out.println(curso.getNome());
+		if (curso != null) {
+			curso.setNome("ADS NOVO");
+			repository.update(curso);
+
+			Curso newCurso = repository.show(1);
+			System.out.println(newCurso.getNome());
+		}
+		System.out.println("--------------FIM UPDATE");
+
+		// Delete
+		curso = repository.show(2);
+		System.out.println(curso.getNome());
+		if (curso != null) {
+			repository.delete(curso);
+		}
+		System.out.println("--------------FIM DELETE");
+
+		cursos = repository.list();
+		for (Curso cursoItem : cursos) {
+			System.out.println(cursoItem.getNome());
+		}
 
 	}
 
